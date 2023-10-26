0dd5568feea345ad791db7b26afa4f22abd83714
murod383 <130088278+murod383@users.noreply.github.com>
2023-10-26 04:27:36 +0100

    INFO; this is 1st homework

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+System.Console.Write("Doira radiusini kiriting: ");
+double radius = Convert.ToDouble(Console.ReadLine());
+
+double doiraYuzi = Math.PI * Math.Pow(radius, 2);
+double aylanaUzunligi = 2 * Math.PI * radius;
+
+System.Console.WriteLine($"S={doiraYuzi}, L={aylanaUzunligi}");
+
+
